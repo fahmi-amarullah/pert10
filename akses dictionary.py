cec9bec4428f7f2a307abf5ce48d43e5ecb7af09
unknown <fachmiamrullah8@gmail.com>
2025-11-20 14:51:08 +0700

    first commit

---
+++ akses dictionary.py
@@ -0,0 +1,10 @@
+dict = {'nama': 'adit', 'class': 'a25', 'umur': '18'}
+
+nama = dict["nama"]
+umur = dict["umur"]
+
+umur = dict.get("umur")
+
+umur = dict.get("umur", "alternative value")
+
+print (dict['class'])
---
+++ dictionary.py
@@ -0,0 +1,11 @@
+a = {} #empty dict
+a = {'n1': 100, 'n2': 20, 'n3': 7}
+
+print(a['n2']) #print element with key 'n2'
+print(a.keys()) #print all key from dict
+print(a.values()) #print all value form dict
+print(a.items()) #print list of tuple (key, value)from dict
+a['n1'] = 900 #change the ite,
+a['n2'] = 200
+
+print (a)
